8d47c998683c74134be8eddddd8f17a6939849b4
imagicgang <70812896+imagicgang@users.noreply.github.com>
2020-09-06 00:23:56 +0700

    Exercise 4
    For Python 3 (BorntoDEV)

---
+++ Exercise_4_Thanapon_P.py
@@ -0,0 +1,18 @@
+student_id = 634055
+name = "Thanapon" 
+surname = "Pumeechokchai"
+''' คะแนนแต่ละวิชา '''
+f = 80
+g = 65
+i = 90
+c = 76
+print("Python University")
+print("Welcome to Student Infomation System")
+print()
+print("Name:",name,surname)
+print("Student ID:",student_id)
+print("-----------Your Score----------")
+print("Foundation English :",f)
+print("General Business :",g)
+print("Introduction to Computer Systems :",i)
+print("Computer Programming :",c)
