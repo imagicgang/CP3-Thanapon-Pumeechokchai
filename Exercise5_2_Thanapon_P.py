972548b3cb83a898ec0bb51b8a48aa43b5d8777b
imagicgang <70812896+imagicgang@users.noreply.github.com>
2020-09-06 22:04:32 +0700

    Exercise 5.2
    Velocity Calculate Program

---
+++ Exercise5_2_Thanapon_P.py
@@ -0,0 +1,7 @@
+print("Velocity Calculate Program")
+print("--------------------")
+s = float(input("Input Distance (km): "))
+t = float(input("Input Time (hr): "))
+v = s/t*5/18 # Velocity in (m/s)
+print("--------------------")
+print("Velocity is",s/t,"(km/hr) or",v,"(m/s)")
